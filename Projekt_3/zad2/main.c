b2454d14eeeb067be6d5c3aeb5a2c85531228a3e
Piotr Wojtyś <Pituuuss@gmail.com>
2018-03-26 23:34:04 +0200

    fix fork proglem

--- Projekt_3/zad2/main.c
+++ Projekt_3/zad2/main.c
@@ -1,67 +1,55 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <memory.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-void parse_args(char *buff, char **args)
-{
-    int i = 0;
-    char *p = strtok(buff, " \n\t");
-    while (p != NULL)
-    {
-        args[i++] = p;
-        p = strtok(NULL, " \n\t");
-    }
-    args[i++] = NULL;
+void parse_args(char *buff, char **args) {
+  int i = 0;
+  char *p = strtok(buff, " \n\t");
+  while (p != NULL) {
+    args[i++] = p;
+    p = strtok(NULL, " \n\t");
+  }
+  args[i++] = NULL;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  FILE *fp = fopen("programs.txt", "r");
+  char buff[255];
+  char *args[255];
+  int status = 0;
 
-    FILE *fp = fopen("programs.txt", "r");
-    char buff[255];
-    char *args[255];
-    int status = 0;
+  while (fgets(buff, 255, fp) != NULL) {
+    parse_args(buff, args);
 
-    while (fgets(buff, 255, fp) != NULL)
-    {
-        parse_args(buff, args);
-
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            perror("fork failed");
-            return EXIT_FAILURE;
-        }
-        else if (pid > 0)
-        {
-            printf("starting new process %s on pid %d\n", args[0], pid);
-            execvp(args[0], args);
-            return EXIT_SUCCESS;
-        }
-        printf("looking for process on pid %d\n", pid);
-        if (waitpid(pid, &status, 0) == -1)
-        {
-            perror("waitpid failed");
-            return EXIT_FAILURE;
-        }
-        // wait(&status);
-        const int es = WEXITSTATUS(status);
-
-        printf("exit status was %d %d\n", status, es);
-
-        if (es)
-        {
-            printf("succeded: %s\n", args[0]);
-        }
-        else
-        {
-            printf("failed: %s\n", args[0]);
-            break;
-        }
+    pid_t pid = fork();
+    if (pid == -1) {
+      perror("fork failed");
+      return EXIT_FAILURE;
+    } else if (pid == 0) {
+      printf("starting new process %s on pid %d parent pid: %d\n", args[0],
+             getpid(), getppid());
+      execvp(args[0], args);
+      exit(EXIT_SUCCESS);
     }
 
-    fclose(fp);
-    return 0;
+    if (waitpid(pid, &status, 0) == -1) {
+      perror("waitpid failed");
+      return EXIT_FAILURE;
+    }
+    const int exit_status = WEXITSTATUS(status);
+
+    printf("exit status was %d\n", exit_status);
+
+    if (exit_status) {
+      printf("failed: %s\n", args[0]);
+      break;
+    } else {
+      printf("succeed: %s\n", args[0]);
+    }
+  }
+
+  fclose(fp);
+  return 0;
 }
